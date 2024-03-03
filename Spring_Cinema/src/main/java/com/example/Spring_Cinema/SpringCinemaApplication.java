4f6123e79203902353876bbb56dfd0062458b37c
Jean <jean-ata@hotmail.co.uk>
2024-03-03 21:26:20 +0000

    added initial files

---
+++ Spring_Cinema/src/main/java/com/example/Spring_Cinema/SpringCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Cinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCinemaApplication.class, args);
+	}
+
+}
