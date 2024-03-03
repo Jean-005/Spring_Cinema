77f3876a21f113a551bd31d485a0f5e4274f103a
Jean <jean-ata@hotmail.co.uk>
2024-03-03 21:44:50 +0000

    addded properties to Movie Controller and Services

---
+++ Spring_Cinema/src/main/java/com/example/Spring_Cinema/controllers/MovieController.java
@@ -0,0 +1,31 @@
+package com.example.Spring_Cinema.controllers;
+
+import com.example.Spring_Cinema.models.Movie;
+import com.example.Spring_Cinema.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+
+    @Autowired
+    MovieService movieService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
+        Movie movie = movieService.getMovieById(id);
+        return new ResponseEntity<>(movie, OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Movie> addNewMovie(@RequestBody Movie movie) {
+        movieService.addNewMovie(movie);
+        return new ResponseEntity<>(movie, CREATED);
+
+    }
+
+}
--- Spring_Cinema/src/main/java/com/example/Spring_Cinema/services/MovieService.java
+++ Spring_Cinema/src/main/java/com/example/Spring_Cinema/services/MovieService.java
@@ -1,5 +1,29 @@
 package com.example.Spring_Cinema.services;
 
+import com.example.Spring_Cinema.models.Movie;
+import com.example.Spring_Cinema.repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class MovieService {
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Movie getMovieById(long id) {
+        return movieRepository.findById(id).get();
+    }
+
+    public Movie addNewMovie(Movie movie) {
+        movieRepository.save(movie);
+        return movie;
+    }
+
 }
